ea6a40c30a50ef5e375c5109e9c216a970529212
gapur <kaloycant@gmail.com>
2022-06-15 23:43:05 +0300

    19 finished

---
+++ 19/index.js
@@ -0,0 +1,45 @@
+// 19. Практика, ч.1. Начинаем создавать приложение
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let lastWatchedMovie1 = prompt("Один из последних просмотренных фильмов?", "");
+let lastWatchedMovieRate1 = +prompt("На сколько оцените его?", "");
+let lastWatchedMovie2 = prompt("Один из последних просмотренных фильмов?", "");
+let lastWatchedMovieRate2 = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastWatchedMovie1] = lastWatchedMovieRate1;
+personalMovieDB.movies[lastWatchedMovie2] = lastWatchedMovieRate2;
+
+console.log(personalMovieDB);
