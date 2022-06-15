63278c03e6942b989c47d6dfd2276eb770653804
gapur <kaloycant@gmail.com>
2022-06-15 23:54:59 +0300

    #20 finished

---
+++ 20/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let num = 50;
+
+switch (num) {
+  case 49: 
+    console.log("Wrong!");
+    break;
+  case 100: 
+    console.log("Wrong!");
+    break;
+  case 51:
+    console.log("Exactly!");
+    break;
+  default:
+    console.log("Soory, maybe next time...");
+}
