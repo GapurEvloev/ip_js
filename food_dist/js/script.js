475c2bf670f7d27319426b824255ad891d23bc75
gapur <kaloycant@gmail.com>
2022-06-24 21:01:47 +0300

    #64 fin

---
+++ food_dist/js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tabheader__item"),
+            tabsContent = document.querySelectorAll(".tabcontent"),
+            tabsParent = document.querySelector(".tabheader__items");
+
+    function hideTabContent () {
+        tabsContent.forEach(item => {
+            // item.style.display = "none";
+            item.classList.add("hide");
+            item.classList.remove("show", "fade");
+        });
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+
+    function showTabContent (i = 0) {
+        // tabsContent[i].style.display = "block";
+        tabsContent[i].classList.remove("hide");
+        tabsContent[i].classList.add("show", "fade");
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener("click", (event) => {
+        const target = event.target;
+
+        tabs.forEach((item, i) => {
+            if (target && target === item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    });
+});
