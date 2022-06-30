c4da805735a3327ef8f96d6cd25cc2c8c22339ad
gapur <kaloycant@gmail.com>
2022-07-01 02:22:22 +0300

    #88 задачи

--- 88/index.js
+++ 88/index.js
@@ -49,3 +49,65 @@ const objectEntries = Object.entries(object)
 
 console.log(objectEntries);
 // 88. Методы перебора массивов end
+
+// 88. Exercises start
+const films = [
+  {
+      name: 'Titanic',
+      rating: 9
+  },
+  {
+      name: 'Die hard 5',
+      rating: 5
+  },
+  {
+      name: 'Matrix',
+      rating: 8
+  },
+  {
+      name: 'Some bad film',
+      rating: 4
+  }
+];
+
+function showGoodFilms(arr) {
+  return arr.filter(item => item.rating >= 8);
+}
+
+function showListOfFilms(arr) {
+  return arr.map(item => item.name).join(", ");
+}
+
+function setFilmsIds(arr) {
+  return arr.map((item, i) => {
+    item.id = i;
+    return item;
+  });
+}
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+  return arr.every(item => item.hasOwnProperty("id"));
+}
+
+// =================================================================================================
+
+const funds = [
+  {amount: -1400},
+  {amount: 2400},
+  {amount: -1000},
+  {amount: 500},
+  {amount: 10400},
+  {amount: -11400}
+];
+
+const getPositiveIncomeAmount = (data) => {
+  return data.filter(item => item.amount > 0).reduce((sum, current) => sum + current.amount, 0);
+};
+
+const getTotalIncomeAmount = (data) => {
+  return data.some(item => item.amount < 0) ? data.reduce((sum, current) => sum + current.amount, 0) : getPositiveIncomeAmount(data);
+};
+
+// 88. Exercises end
