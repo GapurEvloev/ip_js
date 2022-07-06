ab01beb42356a401267651055b63ade431bea217
gapur <kaloycant@gmail.com>
2022-07-06 18:56:43 +0300

    #111 fin

--- 110-jquery/script.js
+++ 110-jquery/script.js
@@ -16,3 +16,34 @@ $(document).ready(function() {
     }, 3000);
   });
 });
+// 110 end
+
+// 111 start
+function* generator() {
+  yield "S";
+  yield "c";
+  yield "r";
+  yield "i";
+  yield "p";
+  yield "t";
+}
+
+const str = generator();
+
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+
+function* count(n) {
+  for (let i = 0; i < n; i++) {
+    yield i;
+  }
+}
+
+for (const key of count(7)) {
+  console.log(key);
+}
+// 111 end
