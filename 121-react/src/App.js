0bf3ee27cc4c97487109d085dea0459d100073d5
gapur <kaloycant@gmail.com>
2022-07-20 21:01:49 +0200

    #125 fin

--- 121-react/src/App.js
+++ 121-react/src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// const elem = React.createElement('h2', {className: 'greeting icon'}, "Hello world");
+
+class Field extends React.Component {
+  render() {
+    const holder = "Enter here:";
+    const styleField = {
+      width: "400px",
+    };
+    return <input type="text" placeholder={holder} style={styleField}/>
+  };
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Field/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
