d72a7e49c2f934187a872d4b68919e5956548400
gapur <kaloycant@gmail.com>
2022-07-21 19:40:33 +0200

    #132 fin - events in react

--- 130-class-react/src/App.js
+++ 130-class-react/src/App.js
@@ -6,25 +6,48 @@ class WhoIAm extends React.Component {
     super(props);
     this.state = {
       years: 30,
-      text: "+++"
+      text: "+++",
+      position: "",
     };
+    this.nextYear = this.nextYear.bind(this); // задаем контекст для метода nextYear
   }
 
-  nextYear = () => {
+  nextYear() {
     this.setState(state => ({
       years: state.years + 1,
     }));
   };
+  
+  prevYear() {
+    this.setState(state => ({
+      years: state.years - 1,
+    }));
+  };
+
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value,
+    });
+  };
 
   render() {
     const { name, surname, link } = this.props;
+    const { years, position} = this.state;
     return (
       <div>
+        <button onClick={() => this.prevYear()}>{"---"}</button>
         <button onClick={this.nextYear}>{this.state.text}</button>
         <h1>
-          My name is {name}, surname - {surname}, age - {this.state.years}
+          My name is {name}, surname - {surname}, 
+          age - {years}, 
+          position - {position}
         </h1>
         <a href={link}>My profile</a>
+        <form action="">
+          <span>Enter position</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, "some color")} />
+        </form>
       </div>
     );
   }
