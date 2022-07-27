99a43eca7bc3b90df1c71946c9ac4db9345815df
gapur <kaloycant@gmail.com>
2022-07-27 23:54:58 +0200

    start

---
+++ 148-mrvl/src/components/app/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AppHeader from "../appHeader/AppHeader";
+
+function App() {
+  return (
+    <div className="app">
+      <AppHeader/>
+    </div>
+  );
+}
+
+export default App;
