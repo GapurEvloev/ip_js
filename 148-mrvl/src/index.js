7a1f3f129b5a5c71b1d3f47aafecc0ab766e6fa5
gapur <kaloycant@gmail.com>
2022-07-28 12:58:22 +0200

    #149 fin - data transform & random char

--- 148-mrvl/src/index.js
+++ 148-mrvl/src/index.js
@@ -2,18 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style.scss";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => {
-  console.log(res);
-})
-marvelService.getCharacter(1011052).then(res => {
-  res.data.results.forEach(item => {
-    console.log(item.name);
-  })
-})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
