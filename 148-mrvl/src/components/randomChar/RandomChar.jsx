0e2ea0e26fd0a6013a35d977d90a53227659c290
gapur <kaloycant@gmail.com>
2022-07-28 14:37:45 +0200

    #150 fin - spinner and catch errors

--- 148-mrvl/src/components/randomChar/RandomChar.jsx
+++ 148-mrvl/src/components/randomChar/RandomChar.jsx
@@ -1,8 +1,11 @@
 import { Component } from "react";
 
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
-import MarvelService from "../../services/MarvelService";
 
 class RandomChar extends Component {
   constructor(props) {
@@ -13,55 +16,46 @@ class RandomChar extends Component {
 
   state = {
     char: {},
+    loading: true,
+    error: false,
   };
 
   marvelService = new MarvelService();
 
   onCharLoaded = (char) => {
-    this.setState({ char });
+    this.setState({ char, loading: false });
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
   };
 
   updateRandomChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    this.marvelService.getCharacter(id).then(this.onCharLoaded);
+    this.marvelService
+      .getCharacter(id)
+      .then(this.onCharLoaded)
+      .catch(this.onError);
   };
+
   updateAllChars = () => {
-    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
     this.marvelService.getAllCharacters().then((res) => console.log(res));
   };
 
   render() {
-    const {
-      char: { name, description, thumbnail, homepage, wiki },
-    } = this.state;
-
-    const updateDescription = (str, length) => {
-      return str
-        ? str.length > length
-          ? `${str.substring(0, length)}...`
-          : str
-        : "Marvel character"
-    }
+    const { char, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? <View char={char}/> : null;
 
     return (
       <div className="randomchar">
-        <div className="randomchar__block">
-          <img src={thumbnail} alt={name} className="randomchar__img" />
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">
-              {updateDescription(description, 150)}
-            </p>
-            <div className="randomchar__btns">
-              <a href={homepage} className="button button__main">
-                <div className="inner">homepage</div>
-              </a>
-              <a href={wiki} className="button button__secondary">
-                <div className="inner">Wiki</div>
-              </a>
-            </div>
-          </div>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
         <div className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!
@@ -78,5 +72,35 @@ class RandomChar extends Component {
     );
   }
 }
+
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki } = char;
+
+  const updateDescription = (str, length) => {
+    return str
+      ? str.length > length
+        ? `${str.substring(0, length)}...`
+        : str
+      : "Marvel character";
+  };
+
+  return (
+    <div className="randomchar__block">
+      <img src={thumbnail} alt={name} className="randomchar__img" />
+      <div className="randomchar__info">
+        <p className="randomchar__name">{name}</p>
+        <p className="randomchar__descr">{updateDescription(description, 150)}</p>
+        <div className="randomchar__btns">
+          <a href={homepage} className="button button__main">
+            <div className="inner">homepage</div>
+          </a>
+          <a href={wiki} className="button button__secondary">
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RandomChar;
