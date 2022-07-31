f465667b3d94ec67ad58661e05cad7a2fcfda544
gapur <kaloycant@gmail.com>
2022-07-31 21:46:39 +0200

    #160 fin - portals

--- 130-class-react/src/App.js
+++ 130-class-react/src/App.js
@@ -1,31 +1,29 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import { Container } from "react-bootstrap";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import styled from "styled-components";
+import styled from "styled-components";
 
 class Form extends Component {
-  // myRef = React.createRef();
-
-  // componentDidMount() {
-  //   this.myRef.current.focus();
-  // };
-
-  setInputRef = (el) => {
-    this.myRef2 = el;
+  state = {
+    advOpen: false,
   };
 
+  componentDidMount() {
+    setTimeout(this.handleClick, 3000);
+  }
+
   handleClick = () => {
-    this.myRef2.focus();
-    console.log(this.myRef2);
+    this.setState({advOpen: !this.state.advOpen});
   }
 
   render() {
     return (
       <Container>
-        <form className="w-50 border mt-5 p-3 m-auto">
+        <form onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto" style={{overflow: 'hidden', position: 'relative'}}>
           <div className="mb-3">
             <label htmlFor="exampleFormControlInput1" className="form-label">
               Email address
@@ -44,11 +42,31 @@ class Form extends Component {
             </label>
             <textarea onClick={this.handleClick} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
           </div>
+          { this.state.advOpen ?
+            <Portal>
+              <Message>Hello</Message>
+            </Portal> : null
+          }
         </form>
       </Container>
     );
   }
 }
+
+const Portal = (props) => {
+  const node = document.createElement('div');
+  document.body.appendChild(node);
+  return ReactDOM.createPortal(props.children, node);
+};
+
+const Message = styled.div`
+  width: 500px;
+  height: 150px;
+  background: red;
+  position: absolute;
+  right: 0%;
+  bottom: 0%;
+`; 
 
 function App() {
   return <Form />;
