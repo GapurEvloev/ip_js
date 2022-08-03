36878a2f2f4816cafa9d4453ed62bc8a97f7a842
gapur <kaloycant@gmail.com>
2022-08-03 02:20:03 +0200

    #166 - useMemo fin

--- 163/src/App.js
+++ 163/src/App.js
@@ -1,59 +1,11 @@
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback, useEffect, useMemo, useState} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-// class Slider extends Component {
-//
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
-//
-//     componentDidMount() {
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
-//
-//     componentDidUpdate(prevProps, prevState, snapshot) {
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
-//
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }));
-//     }
-//
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }));
-//     }
-//
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
-// }
+const countTotal = (num) => {
+    console.log("counting total")
+    return num + 10;
+}
 
 const SliderHook = () => {
     const [slide, setSlide] = useState(0);
@@ -87,14 +39,22 @@ const SliderHook = () => {
         setAutoplay(autoplay => !autoplay);
     }
 
+    const total = useMemo(() => countTotal(slide), [slide]);
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? "red" : "black",
+    }), [slide])
+
+    useEffect(() => console.log(style), [style])
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
 
                 <Slide getSomeImg={getSomeImg}/>
 
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
                 <div className="text-center buttons mt-3">
                     <button
                         className="btn btn-primary me-2"
