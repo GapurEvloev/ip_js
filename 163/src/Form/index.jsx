5a94087e12633069ccd2aa6149e5ba183fa581a1
gapur <kaloycant@gmail.com>
2022-08-04 15:56:55 +0200

    #169 - custom hooks

--- 163/src/Form/index.jsx
+++ 163/src/Form/index.jsx
@@ -1,26 +1,46 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useRef, useState} from "react";
 import { Container } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+function useInputWithValidate(initValue) {
+  const [value, setValue] = useState(initValue);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  const validateInput = () => {
+    return value.search(/\d/) >= 0;
+  };
+
+  return {value, onChange, validateInput};
+}
+
 const Form = () => {
-  const [text, setText] = useState("");
+  const input = useInputWithValidate("");
+  const textArea = useInputWithValidate("");
+
   const myRef = useRef(1);
 
-  useEffect(() => console.log(myRef.current))
+  // React.useEffect(() => console.log(myRef.current));
+
+  const color = input.validateInput() ? "text-danger" : null;
+  const colorTextArea = textArea.validateInput() ? "text-danger" : null;
 
   return (
     <Container>
       <form className="w-50 border mt-5 p-3 m-auto" style={{overflow: 'hidden', position: 'relative'}}>
         <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
+          <input type="text" value={`${input.value} / ${textArea.value}`} className="form-control" readOnly />
+          <label htmlFor="exampleFormControlInput1" className="form-label mt-3">
             Email address
           </label>
           <input
-            onChange={(e) => setText(e.target.value)}
-            value={text}
+            onChange={input.onChange}
+            value={input.value}
             type="email"
-            className="form-control"
+            className={`form-control ${color}`}
             id="exampleFormControlInput1"
             placeholder="name@example.com"
           />
@@ -29,7 +49,7 @@ const Form = () => {
           <label htmlFor="exampleFormControlTextarea1" className="form-label">
             Example textarea
           </label>
-          <textarea onClick={() => myRef.current + 1} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <textarea onClick={() => myRef.current + 1} onChange={textArea.onChange} value={textArea.input} className={`form-control ${colorTextArea}`} id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
       </form>
     </Container>
