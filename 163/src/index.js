72daf5b9dd95b7e89f5dde16a2e00663b2537a12
gapur <kaloycant@gmail.com>
2022-08-08 00:39:39 +0200

    #179 - useReducer fin

--- 163/src/index.js
+++ 163/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App178Context.js";
+// import App from "./App178Context.js";
+import App from "./App179UseReducer.js";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
