dd92a475cce55967cf75dbf9b3d50855f37627c6
gapur <kaloycant@gmail.com>
2022-08-11 18:28:12 +0200

    example with Transition

--- 182-React-Transition-Group/src/App.js
+++ 182-React-Transition-Group/src/App.js
@@ -1,42 +1,90 @@
-import {useState} from 'react';
-import {Container} from 'react-bootstrap';
-import './App.css';
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import Transition from "react-transition-group/Transition";
+import "./App.css";
 
 const Modal = (props) => {
+  const duration = 300;
 
-    return (
-        <div className="modal mt-5 d-block">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Typical modal window</h5>
-                    <button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                    <p>Modal body content</p>
-                </div>
-                <div className="modal-footer">
-                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
-                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
-                </div>
-                </div>
+  const defaultStyle = {
+    transition: `all ${duration}ms ease-in-out`,
+    opacity: 0,
+    transform: "translateY(200px)",
+    visibility: 'hidden',
+  };
+
+  const transitionStyles = {
+    entering: { opacity: 1, visibility: 'visible', transform: "translateY(0)", },
+    entered: { opacity: 1, visibility: 'visible', transform: "translateY(0)", },
+    exiting: { opacity: 0, visibility: 'hidden', transform: "translateY(200px)", },
+    exited: { opacity: 0, visibility: 'hidden', transform: "translateY(200px)", },
+  };
+
+  return (
+    <Transition 
+      in={props.show}
+      timeout={duration}
+      // unmountOnExit
+      onEnter={() => props.setShowTrigger(false)}
+      onExited={() => props.setShowTrigger(true)}
+    >
+      {(state) => (
+        <div 
+          style={{
+            ...defaultStyle,
+            ...transitionStyles[state]
+          }}
+          className="modal mt-5 d-block"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Typical modal window</h5>
+                <button
+                  onClick={() => props.onClose(false)}
+                  type="button"
+                  className="btn-close"
+                  aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <p>Modal body content</p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  onClick={() => props.onClose(false)}
+                  type="button"
+                  className="btn btn-secondary">
+                  Close
+                </button>
+                <button
+                  onClick={() => props.onClose(false)}
+                  type="button"
+                  className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
             </div>
+           </div>
         </div>
-    )
-}
+      )}
+    </Transition>
+  );
+};
 
 function App() {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showTrigger, setShowTrigger] = useState(true);
 
-    return (
-        <Container>
-            {showModal ? <Modal onClose={setShowModal}/> : null}
-            <button 
-                type="button" 
-                className="btn btn-warning mt-5"
-                onClick={() => setShowModal(true)}>Open Modal</button>
-        </Container>
-    );
+  return (
+    <Container>
+      <Modal onClose={setShowModal} show={showModal} setShowTrigger={setShowTrigger} />
+      {
+        showTrigger ? <button type="button" className="btn btn-warning mt-5" onClick={() => setShowModal(true)}>
+          Open Modal
+        </button> : null
+      }
+    </Container>
+  );
 }
 
 export default App;
